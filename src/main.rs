732ddecc1f82b5b27958400d125c5faec3ed5815
Matt Weller <hellmitre@gmail.com>
2024-01-12 21:06:30 -0800

    almost retrieves same short url for identical orig

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
 #[macro_use]
 extern crate rocket;
+mod url;
+
+use rocket::response::Redirect;
+use rusqlite::Error;
+
+const HOST_PREFIX: &str = "http://192.168.6.151:8192/";
 
 #[get("/hello/<name>/<age>")]
 fn hello(name: &str, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+#[post("/new", data = "<orig_url>")]
+fn new(orig_url: String) -> String {
+    HOST_PREFIX.to_string() + url::new(orig_url).unwrap().as_str()
 }
 
+#[get("/<short_url>")]
+fn retrieve(short_url: String) {
+    Redirect::to(url::retrieve(short_url).unwrap());
+}
+
+#[get("/error")]
+fn uh_oh() -> String {
+    format!("{}", "500")
+}
+
+#[launch]
+fn rocket() -> _ {
+    let f = url::create_tables();
+    match f {
+        Ok(_s) => _s,
+        Err(e) => {
+            rocket::build().mount("/", routes![uh_oh]);
+            Redirect::to("/error");
+        }
+    };
+    rocket::build().mount("/", routes![hello, retrieve, new, uh_oh])
+}
--- src/url.rs
+++ src/url.rs
@@ -1,32 +1,98 @@
 extern crate rusqlite;
+use rand::{self, Rng};
+use rocket::response::Redirect;
+use std::borrow::Cow;
+
 use rusqlite::{Connection, Result};
 
-#[derive(Debug)]
-struct Url {
-    id: u32,
-    orig_url: String,
-    short_url: String,
+const SHORT_URL_LEN: usize = 6;
+
+#[derive(Debug, Default)]
+pub struct ShortUrl<'a>(Cow<'a, str>);
+
+impl ShortUrl<'_> {
+    pub fn new(size: usize) -> ShortUrl<'static> {
+        const BASE62: &[u8] = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        let mut id = String::with_capacity(size);
+        let mut rng = rand::thread_rng();
+        for _ in 0..size {
+            id.push(BASE62[rng.gen::<usize>() % 62] as char);
+        }
+
+        ShortUrl(Cow::Owned(id))
+    }
+    pub fn to_string(&self) -> String {
+        format!("{}", &self.0.to_string())
+    }
+
+    pub fn from_string(&mut self, short_url: String) -> &ShortUrl {
+        self.0 = std::borrow::Cow::Borrowed(short_url.as_str());
+        self
+    }
 }
 
-fn create() -> Result<()> {
-    let conn = Connection::open_in_memory()?;
+#[derive(Debug, Default)]
+struct Url<'a> {
+    orig_url: String,
+    short_url: ShortUrl<'a>,
+}
+
+pub(crate) fn retrieve(short_url: String) -> Result<String> {
+    let conn = Connection::open("aqlink.db")?;
+
+    let mut stmt = conn.prepare("SELECT orig_url FROM urls WHERE short_url=:short_url LIMIT 1")?;
+    let urls_iter = stmt.query_map(&[(":short_url", short_url.as_str())], |row| {
+        Ok(Url {
+            orig_url: row.get(0)?,
+            short_url: ShortUrl::default(),
+        })
+    })?;
+
+    for u in urls_iter {
+        return Ok(u.unwrap().orig_url);
+    }
+    Ok("foo".to_string())
+}
+
+pub(crate) fn create_tables() -> Result<()> {
+    let conn = Connection::open("aqlink.db")?;
 
     conn.execute(
         "CREATE TABLE IF NOT EXISTS urls (
-        orig_url text,
-        short_url text,
-        uid unsigned big int
-    )",
+        orig_url text primary key,
+        short_url text)",
         (),
     )?;
+
+    Ok(())
+}
+
+pub(crate) fn new(orig_url: String) -> Result<String> {
+    let short_url = ShortUrl::new(SHORT_URL_LEN);
     let url = Url {
-        id: 1,
-        orig_url: "https://lifehacker.com/some/path",
-        short_url: "https://u.aql.ink/1q2w3e4r",
+        orig_url: orig_url.clone(),
+        short_url,
     };
+
+    let conn = Connection::open("aqlink.db")?;
+
+    let mut stmt = conn.prepare("SELECT short_url FROM urls WHERE orig_url=:orig_url LIMIT 1")?;
+    let urls_iter = stmt.query_map(&[(":orig_url", orig_url.as_str())], |row| {
+        Ok(Url {
+            short_url: ShortUrl::default(),
+            orig_url: "".to_string(),
+        })
+    })?;
+
+    for u in urls_iter {
+        return Ok(u.unwrap().orig_url);
+    }
 
     conn.execute(
         "INSERT INTO urls (orig_url, short_url) VALUES (?1, ?2)",
-        (&url.orig_url, &url.short_url),
+        (&url.orig_url, &url.short_url.to_string()),
     )?;
+
+    Ok(url.short_url.to_string())
 }
